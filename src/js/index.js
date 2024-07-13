bdda5a4c1796b9cd2c1ea9342e905187c139e533
Thaísa Vieira <thaisavieira.tech@proton.me>
2024-07-12 23:28:06 -0300

    feat: adding new activities via form, marking activity progress status, and date formatting.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
 const formatter = (date) => {
-    //console.log(dayjs(date).format('dddd'))
-    //formatter(new Date('2024-04-01'))
-
     return{
         day:{
             numeric: dayjs(date).format('DD'),
@@ -19,7 +16,7 @@ const formatter = (date) => {
 const activity = {
     name:'Feeding cats',
     date:new Date("2024-07-08 06:00"),
-    finished:true,
+    finished:false,
 }
 
 let activities = [
@@ -32,15 +29,19 @@ let activities = [
     {
         name:'Watch cats take over the world',
         date: new Date("2024-07-09 15:00"),
-        finished:true
+        finished:false
     },
 ]
 
-//activities = []
 
 const createActivityItem = (activity) => {
 
-    let input ='<input type="checkbox" '
+    let input =`
+    <input 
+    onchange = "concludeActivity(event)"
+    value="${activity.date}"
+    type="checkbox" 
+    `
 
     if(activity.finished){
         input += 'checked'
@@ -67,6 +68,7 @@ const createActivityItem = (activity) => {
 
 const updateActivityList = () => {
     const section = document.querySelector('section');
+    section.innerHTML=''
 
 //check if my list is empty:
 if(activities.length == 0){
@@ -82,3 +84,91 @@ for(let activity of activities){
 
 updateActivityList()
 
+const saveActivity = (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+
+    const name = formData.get('activity')
+    const day = formData.get('day')
+    const hour = formData.get('hour')
+    const date = `${day} ${hour}`
+
+    const newActivity = {
+        name,
+        date,
+        finished:false
+    }
+
+    const doesTheActivityExist = activities.find((activity)=>{
+        return activity.date == newActivity.date //if true activity exists
+    })
+
+    if(doesTheActivityExist){
+        return alert('Day/time not available')
+    }
+
+    activities = [newActivity, ...activities]
+    updateActivityList()
+
+
+}
+
+
+const createSelectionDays = () => {
+    const days = [
+        "2024-07-06",
+        "2024-07-07",
+        "2024-07-13",
+        "2024-07-14",
+        "2024-07-20",
+        "2024-07-21",
+        "2024-07-27",
+        "2024-07-28"
+    ]
+
+    let daysSelection =''
+
+    for(let day of days){
+        const format = formatter(day)
+        const displayFormattedDay = `
+        ${format.month},
+        ${format.day.numeric}
+        `
+        
+        daysSelection += `
+        <option value="${day}">${displayFormattedDay}</option>
+        `
+    }
+
+    document.querySelector('select[name="day"]')
+    .innerHTML = daysSelection
+}
+createSelectionDays()
+
+const createSelectionHours = () => {
+    let hoursAvailable = ''
+
+    for(let i = 11; i<19;i++){
+        hoursAvailable += `<option value="${i}:00">${i}:00</option>`
+        hoursAvailable += `<option value="${i}:30">${i}:30</option>`
+    }
+
+    document.querySelector('select[name=hour]').innerHTML = hoursAvailable
+}
+createSelectionHours()
+
+const concludeActivity = (event) => {
+    const input = event.target
+    const inputDate = input.value
+
+    const activity = activities.find((activity) => 
+    {
+        return activity.date == inputDate
+    })
+
+    if(!activity){
+        return
+    }
+
+    activity.finished = !activity.finished
+}
