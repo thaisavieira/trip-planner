6fc077f68e92f4d57dde18530126e117c7487c1c
Thaísa Vieira <thaisavieira.tech@proton.me>
2024-08-01 23:38:05 -0300

    adding animation, responsiveness and general stylization

--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,49 @@
 const formatter = (date) => {
-    return{
-        day:{
+    return {
+        day: {
             numeric: dayjs(date).format('DD'),
-            week:{
+            week: {
                 short: dayjs(date).format('ddd'),
                 long: dayjs(date).format('dddd'),
             }
         },
-        month:dayjs(date).format('MMMM'),
+        month: dayjs(date).format('MMMM'),
         hour: dayjs(date).format('HH:mm')
     }
 }
-    
+
 
 const activity = {
-    name:'Feeding cats',
-    date:new Date("2024-07-08 06:00"),
-    finished:false,
+    name: 'Feeding cats',
+    date: new Date("2024-07-08 06:00"),
+    finished: false,
 }
 
 let activities = [
     activity,
     {
-        name:'Taking a walk to meet neighborhood cats',
+        name: 'Taking a walk',
         date: new Date("2024-07-09 10:00"),
-        finished:false
+        finished: false
     },
     {
-        name:'Watch cats take over the world',
+        name: 'Watch cats take over the world',
         date: new Date("2024-07-09 15:00"),
-        finished:false
+        finished: false
     },
 ]
 
 
 const createActivityItem = (activity) => {
 
-    let input =`
+    let input = `
     <input 
     onchange = "concludeActivity(event)"
     value="${activity.date}"
     type="checkbox" 
     `
 
-    if(activity.finished){
+    if (activity.finished) {
         input += 'checked'
     }
 
@@ -53,10 +53,29 @@ const createActivityItem = (activity) => {
 
 
     return `
-    <div>
+    <div class='card-bg'>
         ${input}
+        <div>
+        <svg class = "active" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6.50008 8.99999L8.16675 10.6667L11.5001 7.33332M17.3334 8.99999C17.3334 13.6024 13.6025 17.3333 9.00008 17.3333C4.39771 17.3333 0.666748 13.6024 0.666748 8.99999C0.666748 4.39762 4.39771 0.666656 9.00008 0.666656C13.6025 0.666656 17.3334 4.39762 17.3334 8.99999Z" stroke="#BEF264" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+        <svg class = "inactive" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.41664 0.818364C8.46249 0.615965 9.53745 0.615965 10.5833 0.818364M10.5833 17.1817C9.53745 17.3841 8.46249 17.3841 7.41664 17.1817M13.6741 2.10086C14.5587 2.70022 15.3197 3.46409 15.9158 4.35086M0.818303 10.5834C0.615904 9.53751 0.615904 8.46255 0.818303 7.4167M15.8991 13.6742C15.2998 14.5588 14.5359 15.3198 13.6491 15.9159M17.1816 7.4167C17.384 8.46255 17.384 9.53751 17.1816 10.5834M2.1008 4.32586C2.70016 3.44131 3.46403 2.68026 4.3508 2.0842M4.3258 15.8992C3.44124 15.2998 2.6802 14.536 2.08414 13.6492" stroke="#A1A1AA" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+
         <span>${activity.name}</span>
-        <time>
+        </div>
+        
+
+        <time class='short'>
+        ${format.day.week.short},
+        ${format.day.numeric} <br>
+        ${format.hour}
+        </time>
+
+        <time class='full'>
         ${format.day.week.long},
         ${format.month}
         ${format.day.numeric},
@@ -68,19 +87,19 @@ const createActivityItem = (activity) => {
 
 const updateActivityList = () => {
     const section = document.querySelector('section');
-    section.innerHTML=''
+    section.innerHTML = ''
 
-//check if my list is empty:
-if(activities.length == 0){
-    section.innerHTML=`<p>No activities registered</p>`
-    return
+    //check if my list is empty:
+    if (activities.length == 0) {
+        section.innerHTML = `<p>No activities registered</p>`
+        return
+    }
+
+    for (let activity of activities) {
+        section.innerHTML += createActivityItem(activity)
+    }
+
 }
-
-for(let activity of activities){
-    section.innerHTML += createActivityItem(activity)
-}
-
-} 
 
 updateActivityList()
 
@@ -96,14 +115,14 @@ const saveActivity = (event) => {
     const newActivity = {
         name,
         date,
-        finished:false
+        finished: false
     }
 
-    const doesTheActivityExist = activities.find((activity)=>{
+    const doesTheActivityExist = activities.find((activity) => {
         return activity.date == newActivity.date //if true activity exists
     })
 
-    if(doesTheActivityExist){
+    if (doesTheActivityExist) {
         return alert('Day/time not available')
     }
 
@@ -126,29 +145,29 @@ const createSelectionDays = () => {
         "2024-07-28"
     ]
 
-    let daysSelection =''
+    let daysSelection = ''
 
-    for(let day of days){
+    for (let day of days) {
         const format = formatter(day)
         const displayFormattedDay = `
         ${format.month},
         ${format.day.numeric}
         `
-        
+
         daysSelection += `
         <option value="${day}">${displayFormattedDay}</option>
         `
     }
 
     document.querySelector('select[name="day"]')
-    .innerHTML = daysSelection
+        .innerHTML = daysSelection
 }
 createSelectionDays()
 
 const createSelectionHours = () => {
     let hoursAvailable = ''
 
-    for(let i = 11; i<19;i++){
+    for (let i = 11; i < 19; i++) {
         hoursAvailable += `<option value="${i}:00">${i}:00</option>`
         hoursAvailable += `<option value="${i}:30">${i}:30</option>`
     }
@@ -161,12 +180,11 @@ const concludeActivity = (event) => {
     const input = event.target
     const inputDate = input.value
 
-    const activity = activities.find((activity) => 
-    {
+    const activity = activities.find((activity) => {
         return activity.date == inputDate
     })
 
-    if(!activity){
+    if (!activity) {
         return
     }
 
